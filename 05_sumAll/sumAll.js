25017bfd0d9122afa0de17556a855688c607455c
Kevin Le <kevinle29299@gmail.com>
2025-01-20 20:01:39 -0800

    Complete 5th javascript exercise
    Near perfect solution with Odin's!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    
+    // Check if number is less than 0 or if they're decimal
+    if(start < 0 || end < 0 || 
+        !Number.isInteger(start) || !Number.isInteger(end))
+        return "ERROR";
 
+    if(start > end)
+    {
+        const temp = start;
+        start = end;
+        end = temp;
+    }
+
+    let sum = 0;
+    for(let i = start; i <= end; i++)
+        sum+= i;
+    return sum;
 };
 
 // Do not edit below this line
