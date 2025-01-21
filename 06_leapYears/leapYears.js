a03aefaed8500c9e00cb3d45d5350e8047143065
Kevin Le <kevinle29299@gmail.com>
2025-01-20 20:30:31 -0800

    Complete 6th javascript exercise
    Implemented successfully on my own using
    multiple if else statements.
    
    The solutions provide a condensed way of storing
    each modulus into variables, and verifying
    the conditions in one if-statement.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,25 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // Solution 1
+    // if(year % 4 == 0)
+    // {
+    //     if(year % 100 == 0)
+    //     {
+    //         if(year % 400 == 0)
+    //             return true;
+    //         return false;
+    //     }
+    //     return true;
+    // }
+    // return false;
 
+    // Solution 2
+    const divisableBy4 = year % 4 === 0;
+    const divisableBy100 = year % 100 === 0;
+    const divisableBy400 = year % 400 === 0;
+
+    if(divisableBy4 && (!divisableBy100 || divisableBy400))
+        return true;
+    return false;
 };
 
 // Do not edit below this line
