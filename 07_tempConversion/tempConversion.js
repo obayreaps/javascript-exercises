b2f8c70701ac279cdd011b905f7a259585123d46
Kevin Le <kevinle29299@gmail.com>
2025-01-20 21:05:05 -0800

    Complete 7th javascript exercise
    I google the formulas to convert between
    celcius to fahrenheit.
    
    When we use the toFixed method, we turn the
    number into the string. Then we need to use the
    parseFloat function to turn the string back into a
    decimal.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let convertedTemp = (temp - 32) * 5 / 9;
+  if(!Number.isInteger(convertedTemp))
+    return parseFloat(convertedTemp.toFixed(1));
+  return convertedTemp;
 };
 
-const convertToFahrenheit = function() {
+
+
+const convertToFahrenheit = function(temp) {
+  let convertedTemp = (temp * 9 / 5) + 32;
+  if(!Number.isInteger(convertedTemp))
+    return parseFloat(convertedTemp.toFixed(1));
+  return convertedTemp;
 };
 
 // Do not edit below this line
